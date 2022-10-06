57f3fcd02c892591e580f20858ba561a20b88acb
TonyARI <97161584+TonyARI@users.noreply.github.com>
2022-10-06 15:18:58 +0500

    Delete src directory

--- src/Redux/object-helpers.js
+++
@@ -1,9 +0,0 @@
-
-export const updateObjectInArray=(items, id, objPropName, newObjProps)=>{
-    return items.map(u=>{
-        if(u[objPropName]===id) {
-            return {...u, ...newObjProps}
-        }
-        return u
-    })
-}
